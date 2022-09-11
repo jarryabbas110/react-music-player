aa86f067a1adb3828eb5de4b2c59f317768011d7
jarryabbas110 <jarryabbas60@gmail.com>
2022-09-12 03:08:00 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,128 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Intro III",
+      cover: "https://images.hungama.com/c/1/d9f/825/30367189/30367189_300x300.jpg",
+      artist: "Perception - NF",
+      audio: "https://media.hungama.com/c/4/d01/732/30378961/30378961_128.mp3?kad8vQ9K-suH1X3PIhiyOGbCP9zjBrnOORatKSfWOYrpNzRCSsQ19XMlht7spEcdZLjLSfExVSSdk0LR1ggtyNqykChZnsg805EQc9gAmFcfTPrGM_sTmrcuETIgkQcs3LA5Nw",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Outro",
+      cover:
+        "https://images.hungama.com/c/1/d9f/825/30367189/30367189_300x300.jpg",
+      artist: "Perception - NF",
+      audio: "https://media.hungama.com/c/4/f0a/e9b/40282421/40282421_128.mp3?TC5n0gNEB-r-YbjRUEjRrdXj3sQ-WHqQKpbadovowWrpgFke8_5UlU8uRvnVoiliFrz769Q_926xVS5w-JClroiGToD0J-BkCEtJb4c8C1ReoJvYy4vTHtp-CY0a04OeIlZmbQ",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Let You Down",
+      cover:
+        "https://images.hungama.com/c/1/d9f/825/30367189/30367189_300x300.jpg",
+      artist: "Perception - NF",
+      audio: "https://media.hungama.com/c/4/f04/d54/40282424/40282424_128.mp3?oMBzoPeEFzASxEe_7bZoAx2G6B8H63ZqHsqoQgnh9NMXFUqizyqQlxMMq2BbLJjKJGaUEB5wgBymyZfbISayYDsLXLIKqtEsYVKl-t0nxJi6DRnStnayOCjk-V3wuP1k1McOGw",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Search",
+      cover:
+        "https://images.hungama.com/c/1/569/07b/48749353/48749353_300x300.jpg",
+      artist: "The Search - NF",
+      audio: "https://media.hungama.com/c/4/cb4/36f/48749372/48749372_128.mp3?f4BKTKnB12go0-5EApsFVZokhqd6M4lmdgpFS05VAdzlqBjLKtDKrBDVthssf9KdGz1WyewG2dTmml4foEC9QJVHYHWtSF2_Cs5QUUgJrzBAmuFpw81l6bOmo8htUMZSOoJO7A",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "My Life",
+      cover:
+        "https://images.hungama.com/c/1/d9f/825/30367189/30367189_300x300.jpg",
+      artist: "Perception - NF",
+      audio: "https://media.hungama.com/c/4/771/92e/30378966/30378966_128.mp3?iAvQeXL9LBjo6U_2qViebDnt90pqFMgAEIlsv-BP7EA1CS1BjWu9xx06fCEPF6S-VzOlMPwo4GZ7H-62zuhJRzQ4cbxluE297jyEATsP8pI4VAdnCf3bV5-z2h0tmnV28A2wdA",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "My Stress",
+      cover:
+        "https://images.hungama.com/c/1/569/07b/48749353/48749353_300x300.jpg",
+      artist: "The Search - NF",
+      audio: "https://media.hungama.com/c/4/aeb/ccc/48749429/48749429_128.mp3?zCrF98UHV_Uqku6DmPyTirr3n5RAoH6SsZeFRA5heEHL3vfLv02YsyYIX53AjWlYD9PmG13DMVFKCsQ4oZL5ndwOtjZXl3UxqkOBHtPWNOP2qggsd-2K7x7ZVsL2SAnxqVQl0A",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Change",
+      cover:
+        "https://images.hungama.com/c/1/569/07b/48749353/48749353_300x300.jpg",
+      artist: "The Search - NF",
+      audio: "https://media.hungama.com/c/4/9f2/336/48749411/48749411_128.mp3?2GTpcdL36CTLx1DMB0xj57_NFiBXdbrV9wDNjarx9vx7jNS-sO2jX-Rgt80Xl6Fa24v3wOT-OKmm8eXtcwh6CSIBs1MStCvCwh6_KsMzcgyg1rwPQ_DD3djhOJdLNHMD1abQbw",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    //!mysong end!
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
